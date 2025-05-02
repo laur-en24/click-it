d7be9817692693378bf73a2466e871e9d26f28ba
laur-en24 <lauren.nangano@gmail.com>
2025-05-02 19:07:48 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+
+    const images = [
+        "images/IMG_0797.jpeg",
+        "images/IMG_0804.jpeg",
+        "images/IMG_0889.JPG",
+        "images/IMG_0995.jpeg",
+        "images/IMG_1016.jpeg",
+    ];
+
+    let currentIndex = 0;
+
+    function showNextImage() {
+      const img = document.getElementById("display-img");
+
+        img.src = images[currentIndex];
+
+        currentIndex++;
+
+if (currentIndex >= images.length) {
+            currentIndex = 0;
+
+        img.alt = "Image";
+    img.style.maxWidth = "300px";
+    img.style.maxHeight = "300px";
+  
+        }
+    }
+
